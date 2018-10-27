8c07f91783bf9dd4906172d8ea55f9a7bf9cc318
David Pollard <pollardld@gmail.com>
2018-10-27 16:51:06 -0700

    Width of panel

--- pages/regional.js
+++ pages/regional.js
@@ -2,35 +2,32 @@
   var regionID = document.getElementById('regionID').value;
   document.getElementById('map-overlay').classList.add('regional');
   document.getElementById('map-overlay').innerHTML = `
-    <div class="container">
+    <div class="overlay-section">
       <div class="row">
-        <a href="/" class="nav-back">
-          <div class="exit">
-            <img src="./img/i_north_america.svg" />
+        <div class="col">
+          <div class="row">
+            <a href="/" class="nav-back">
+              <div class="exit">
+                <img src="./img/i_north_america.svg" />
+              </div>
+
+              <div class="exit">
+                <img src="./img/i_arrow-left.svg" class="icon_block mb-2" />
+                <span>Back</span>
+              </div>
+            </a>
           </div>
 
-          <div class="exit">
-            <img src="./img/i_arrow-left.svg" class="icon_block mb-2" />
-            <span>Back</span>
-          </div>
-        </a>
-      </div>
-
-      <div class="row">
-        <div class="col-7">
-          <div class="title" id="regionName">${regionName.value}</div>
-          <hr />
-          <div>
-            <p>Enter a species above or check “Average Across All Species” to begin.</p>
+          <div class="row">
+            <div class="col">
+              <div class="title" id="regionName">${regionName.value}</div>
+              <hr />
+              <div>
+                <p>Enter a species above or check “Average Across All Species” to begin.</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="col"></div>
-        <div class="col-4">
-          <button type="button" class="btn-download btn-download-border" data-toggle="modal" data-target="#dataDownloadModal" data-tobedownloaded="${regionID}">
-            <img src="./img/i_download.svg" /> Download Data
-          </button>
-        </div>
-      </div>
     </div>
   `;
 
