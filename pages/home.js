e97a8afb6f5d539610917258476e8cfef3f94f8d
David Pollard <pollardld@gmail.com>
2019-01-04 18:01:31 -0800

    Add about links

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ function render() {
   document.getElementById('map-overlay').innerHTML = `
     <h1>Ocean<br />Adapt</h1>
     <h2>Welcome!
-    <br />Select a region or continent explore changes in marine species distributions.
+    <br />Select a region or the continent to explore changes in marine species distributions
   </h2>
   <button type="button" class="btn-download" data-toggle="modal" data-target="#dataDownloadModal">
     <img src="./img/i_download.svg"> Download Data
